708fb970064120578f4086b39d78f79efeef2e28
JefinFrancis <jefin.francis@objectedge.com>
2021-10-08 17:51:09 +0530

    Assignment5 Done

--- js/view-models/sample.js
+++ js/view-models/sample.js
@@ -2,7 +2,7 @@
  * Core
  */
 import { exportToViewModel } from 'occ-components/widget-core/decorators';
-import { BaseWidget } from 'occ-components/widget-core';
+import { BaseWidget, } from 'occ-components/widget-core';
 
 /**
  * Libraries, Helpers
@@ -20,37 +20,28 @@ export class Sample extends BaseWidget {
   /**
    * On load view model
    */
-  collectionIds = [
-    'jefin_centennial',
-    'jefin_gbc',
-    'jefin_greenball',
-    'jefin_kanati'
-  ];
-
   @exportToViewModel
   brands = ko.observableArray();
 
   getCollection() {
     let data = {};
-    data[ccConstants.CATEGORY_IDS] = this.collectionIds.join(',');
-    console.log("Im Data", data);
+    data[ccConstants.CATEGORY_IDS] = this.$data.collectionItem();
 
     ccRestClient.request(
       ccConstants.ENDPOINT_LIST_COLLECTIONS,
       data,
       (response) => {
         response.map(data => {
-          this.brands.push(new BrandModel(data.displayName, '/file/' + data.categoryImages[0].path, data.route));
+          this.brands.push(new BrandModel(data.displayName, '/file' + data.categoryImages[0].path, data.route));
         });
       },
       (error) => { console.log("Im error", error) }
     );
   }
 
-  constructor() {
+  constructor(widget) {
     //Constructing the BaseWidget
     super();
     this.getCollection();
   }
-
 }
