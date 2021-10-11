5d171075657fab2959f4e37e24536e98dd9aafff
JefinFrancis <jefin.francis@objectedge.com>
2021-10-11 13:10:21 +0530

    Assignment 3 Done

--- js/view-models/sample.js
+++ js/view-models/sample.js
@@ -2,18 +2,17 @@
  * Core
  */
 import { exportToViewModel } from 'occ-components/widget-core/decorators';
-import { BaseWidget, } from 'occ-components/widget-core';
+import { BaseWidget } from 'occ-components/widget-core';
 
 /**
  * Libraries, Helpers
  */
 import ko from 'knockout';
-import ccRestClient from 'ccRestClient';
-import ccConstants from 'ccConstants';
+
 /**
  * Models
  */
-import BrandModel from '../models/brand';
+import CategoryModel from '../models/category';
 
 export class Sample extends BaseWidget {
 
@@ -21,27 +20,18 @@ export class Sample extends BaseWidget {
    * On load view model
    */
   @exportToViewModel
-  brands = ko.observableArray();
+  category = [
+    new CategoryModel('file/general/devices.png', 'DEVICES', 'category/devices'),
+    new CategoryModel('file/general/accessories.png', 'ACCESSORIES', 'category/accessories'),
+    new CategoryModel('file/general/softwares.png', 'SOFTWARE', 'category/software')
+  ];
 
-  getCollection() {
-    let data = {};
-    data[ccConstants.CATEGORY_IDS] = this.$data.collectionItem();
-
-    ccRestClient.request(
-      ccConstants.ENDPOINT_LIST_COLLECTIONS,
-      data,
-      (response) => {
-        response.map(data => {
-          this.brands.push(new BrandModel(data.displayName, '/file' + data.categoryImages[0].path, data.route));
-        });
-      },
-      (error) => { console.log("Im error", error) }
-    );
-  }
-
-  constructor(widget) {
+  constructor() {
     //Constructing the BaseWidget
     super();
-    this.getCollection();
+  }
+
+  beforeAppear() {
+    console.log('[BEFORE APPEAR] Sample');
   }
 }
